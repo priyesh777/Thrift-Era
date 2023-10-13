1c22370076fca6129662f6ced0ed752a01fa2a3e
priyesh777 <priyeshgautam1010@gmail.com>
2023-10-13 01:33:55 -0400

    basic folder setup for thrift-era

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    SafeAreaView,
+    TouchableHighlight,
+    Button,
+    Alert,
+} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const handlePressLink = useCallback(() => {
+        console.log('There is some function to be done::');
+    }, []);
+
+    const handleImageTapped = useCallback(() => {
+        console.log('Image Tapped::');
+    }, []);
+
+    return (
+        <SafeAreaView style={styles.container} >
+            <Text onPress={handlePressLink}>
+                Hello People ! We will work on Thrift Era.
+            </Text>
+            <TouchableHighlight
+                onPress={handleImageTapped}
+            >
+                <Image
+                    fadeDuration={1000}
+                    source={{
+                        width: 300,
+                        height: 200,
+                        uri: "https://picsum.photos/200/300"
+                    }}
+                />
+            </TouchableHighlight>
+            <Button
+                title="Check Basics"
+                onPress={() =>
+                    Alert.alert("Regarding Project", "Did you start with the basics ?", [
+                        { text: "Yes", onPress: () => console.log("Yes") },
+                        { text: "No", onPress: () => console.log("No") },
+                    ])
+                }
+            />
+            <StatusBar style="auto" />
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#4bb389',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
